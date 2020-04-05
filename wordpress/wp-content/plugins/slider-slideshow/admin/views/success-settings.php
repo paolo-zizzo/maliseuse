b5058701e602791600dd889f3d2be6e4b61a3479
colas <jean-davidson.colas@laplateforme.io>
2020-04-05 14:25:12 +0200

    vide

--- wordpress/wp-content/plugins/slider-slideshow/admin/views/success-settings.php
+++
@@ -1,4 +0,0 @@
-<p id="wprls-settings-saved" class="alert alert-success fade in">
-	<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-	<b>Settings</b> saved successfully.
-</p>
